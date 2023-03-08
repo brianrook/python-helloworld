f37f0d305ba3195ce28099bb3f353760790c44a8
Brian Rook <brook@happymoney.com>
2023-03-08 14:41:30 -0700

    fix: cleanup

--- aws.py
+++ aws.py
@@ -1,10 +1,24 @@
 import logging
 import sys
-
-from app.helloworld import HelloWorld
+from pydantic import BaseModel
 
 logger = logging.getLogger("main")
+
 
 def handler(event, context):
     logger.info("handler request in")
     return HelloWorld.getMessage()
+
+
+class MessageResponse(BaseModel):
+    message: str
+
+
+class HelloWorld:
+
+    @staticmethod
+    def getMessage():
+        logger.info("request to helloworld.getmessage")
+        msg_response = MessageResponse(message="Hello World!!")
+        logger.debug("returning msg: %s", msg_response.json())
+        return msg_response
