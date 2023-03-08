c9562e18a3f31990066d716b99f0eb4f5d0ab66f
Brian Rook <brook@happymoney.com>
2023-03-08 15:14:01 -0700

    fix: test update

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ COPY ./requirements.txt .
 RUN pip install --no-cache-dir --upgrade -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
 
 COPY app.py ${LAMBDA_TASK_ROOT}
+ADD ./helloworld/ ${LAMBDA_TASK_ROOT}
 
 CMD [ "app.handler" ]
--- app.py
+++ app.py
@@ -1,22 +1,8 @@
 import logging
-import sys
-from pydantic import BaseModel
+
+from helloworld.helloworld import HelloWorld
 
 logger = logging.getLogger("main")
-
-
-class MessageResponse(BaseModel):
-    message: str
-
-
-class HelloWorld:
-
-    @staticmethod
-    def getMessage():
-        logger.info("request to helloworld.getmessage")
-        msg_response = MessageResponse(message="Hello World!!")
-        logger.debug("returning msg: %s", msg_response.json())
-        return msg_response
 
 
 def handler(event, context):
